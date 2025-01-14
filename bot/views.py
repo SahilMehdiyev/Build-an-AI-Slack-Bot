33657c57ee4f7aca75df3f9f7ea0b56061ce91d1
SahilMehdiyev <branchsahil@gmail.com>
2025-01-14 17:41:25 +0400

    Request url verified slack

--- bot/views.py
+++ bot/views.py
@@ -1,11 +1,24 @@
+import json
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-
-
+from django.views.decorators.http import require_POST
 
 @csrf_exempt
+@require_POST
 def slack_event_endpoint(request):
+    json_data = {}
+    try:
+        json_data = json.loads(request.body.decode('utf-8'))
+    except Exception as e:
+        print(f"Error parsing JSON: {e}")
+        return HttpResponse('Invalid JSON', status=400)
     
-    print(request.body,request.method)
-    return HttpResponse('<h1>Hello world!</h1>',status=200)
-
+    data_type = json_data.get('type')
+    if data_type != 'url_verification':
+        return HttpResponse('Not Allowed', status=400)
+    
+    challenge = json_data.get('challenge')  
+    if challenge is None:
+        return HttpResponse('Not Allowed', status=400)
+    
+    return HttpResponse(challenge, status=200)
