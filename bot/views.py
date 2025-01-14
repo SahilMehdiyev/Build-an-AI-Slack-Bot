eb86577892c4b55d741e6f3ecb476e34046000cc
SahilMehdiyev <branchsahil@gmail.com>
2025-01-14 18:04:29 +0400

    url verification updated

--- bot/views.py
+++ bot/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 
+
+
 @csrf_exempt
 @require_POST
 def slack_event_endpoint(request):
@@ -10,15 +12,20 @@ def slack_event_endpoint(request):
     try:
         json_data = json.loads(request.body.decode('utf-8'))
     except Exception as e:
-        print(f"Error parsing JSON: {e}")
         return HttpResponse('Invalid JSON', status=400)
     
     data_type = json_data.get('type')
-    if data_type != 'url_verification':
+    print(data_type,json_data)
+    allowed_data_type = [
+        'url_verification',
+        'event_callback'
+    ]
+    if data_type not in allowed_data_type:
         return HttpResponse('Not Allowed', status=400)
     
-    challenge = json_data.get('challenge')  
-    if challenge is None:
-        return HttpResponse('Not Allowed', status=400)
+    if data_type == 'url_verification':
+        challenge = json_data.get('challenge')  
+        if challenge is None:
+            return HttpResponse('Not Allowed', status=400)
     
     return HttpResponse(challenge, status=200)
